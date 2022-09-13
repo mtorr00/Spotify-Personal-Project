223d656bce19a1d1a0cca750a02a57b654d28e44
Martin Torrey <hockeymt.torrey@gmail.com>
2022-09-13 14:03:05 -0400

    more tailwind css to AddSearchQuery

--- spotify-app-ui/src/components/AddSearchQuery.js
+++ spotify-app-ui/src/components/AddSearchQuery.js
@@ -19,8 +19,9 @@ const AddSearchQuery = () => {
                 <label className='block text-gray-600 text-sm font-normal'>Email</label>
                 <input type="email" className='h-10 w-96 border mt-2 px-2 py-2'></input>
             </div>
-            <div className='item-center justify-center h-14 w-full'>
+            <div className='item-center justify-center h-14 w-full space-x-4 pt-4'>
                 <button className='rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-2 px-6'>Save</button>
+                <button className='rounded text-white font-semibold bg-red-400 hover:bg-red-700 py-2 px-6'>Save</button>
             </div>
         </div>
     </div>
