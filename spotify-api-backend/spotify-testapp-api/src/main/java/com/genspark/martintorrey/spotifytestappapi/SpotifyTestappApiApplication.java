566605b29adebfd3ee3fb0fe953280e54d57a7c7
Martin Torrey <hockeymt.torrey@gmail.com>
2022-09-13 12:59:30 -0400

    committing all project files initial

---
+++ spotify-api-backend/spotify-testapp-api/src/main/java/com/genspark/martintorrey/spotifytestappapi/SpotifyTestappApiApplication.java
@@ -0,0 +1,13 @@
+package com.genspark.martintorrey.spotifytestappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyTestappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyTestappApiApplication.class, args);
+	}
+
+}
---
+++ spotify-api-backend/spotify-testapp-api/src/test/java/com/genspark/martintorrey/spotifytestappapi/SpotifyTestappApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genspark.martintorrey.spotifytestappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyTestappApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
