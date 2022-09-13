d28b434bbcf33c6a6190a7676618977f62105761
Martin Torrey <hockeymt.torrey@gmail.com>
2022-09-13 13:57:37 -0400

    modified app.js, added new components

--- spotify-app-ui/src/App.js
+++ spotify-app-ui/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import AddSearchQuery from './components/AddSearchQuery'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    
+    <>
+    <Navbar></Navbar>
+    <AddSearchQuery />
+    </>
   );
 }
 
---
+++ spotify-app-ui/src/components/AddSearchQuery.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const AddSearchQuery = () => {
+  return (
+    <div className='flex max-w-2xl mx-auto shadow border-b'>
+        <div className='px-8 py-8'>
+            <div className='font-thin text-2xl tracking-wider'>
+                 <h1>Add New Employee</h1>
+            </div>
+            <div className='item-center justify-center h-14 w-full my-4'>
+                <label className='block text-gray-600 text-sm font-normal'>First Name</label>
+                <input type="text" className='h-10 w-96 border mt-2 px-2 py-2'></input>
+            </div>
+            <div className='item-center justify-center h-14 w-full my-4'>
+                <label className='block text-gray-600 text-sm font-normal'>Last Name</label>
+                <input type="text" className='h-10 w-96 border mt-2 px-2 py-2'></input>
+            </div>
+            <div className='item-center justify-center h-14 w-full my-4'>
+                <label className='block text-gray-600 text-sm font-normal'>Email</label>
+                <input type="email" className='h-10 w-96 border mt-2 px-2 py-2'></input>
+            </div>
+            <div className='item-center justify-center h-14 w-full'>
+                <button className='rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-2 px-6'>Save</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddSearchQuery
